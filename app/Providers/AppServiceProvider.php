c77e835fdb6ae137d9e7e06b78ac57afd26c51a7
ikechukwu <onyenoikechukwu08006@gmail.com>
2026-07-26 08:08:30 +0100

    addiction of Auth Authentcator

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers;
+
+use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
+            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        });
+        Scramble::afterOpenApiGenerated(function (OpenApi $openApi){
+            $openApi->secure(
+                SecurityScheme::http(
+                    'bearer',
+                    'BearerAuth'
+                )
+            );
+        });
+    }
+}
